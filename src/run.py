9981758c6f7b100c7e7a2884c4f39e9b6748849f
Deniz Dilsiz <dkdilsiz@live.com>
2025-11-18 15:30:43 +1100

    refactoring

--- src/run.py
+++ src/run.py
@@ -10,17 +10,20 @@ dotenv.load_dotenv()
 
 generators = [] 
 
+output_folder_name = 'output2'
+ctx_per_record = 2
+num_ctx_records = 50
 
-generators.append(CRIKEYQAGGenerator(export_path='output/CRIKEY_QAG', do_export=True, db=Database(), num_ctx_records=50, ctx_per_record=2, decode_batch_size=4))
+generators.append(CRIKEYQAGGenerator(export_path=f'{output_folder_name}/CRIKEY_QAG', do_export=True, db=Database(), num_ctx_records=num_ctx_records, ctx_per_record=ctx_per_record, decode_batch_size=4))
 
 #Ablations
-generators.append(CRIKEYQAGGenerator(export_path='output/CRIKEY_QAG_noEP', do_export=True, db=Database(), num_ctx_records=50, ctx_per_record=2, decode_batch_size=4, do_entity_pathing=False))
-generators.append(RandomSeedSelectionQAGenerator(export_path='output/RandomSeedSelectionQA', do_export=True, db=Database(), num_ctx_records=50, ctx_per_record=2, decode_batch_size=4))
+generators.append(CRIKEYQAGGenerator(export_path=f'{output_folder_name}/CRIKEY_QAG_noEP', do_export=True, db=Database(), num_ctx_records=num_ctx_records, ctx_per_record=ctx_per_record, decode_batch_size=4, do_entity_pathing=False))
+generators.append(RandomSeedSelectionQAGenerator(export_path=f'{output_folder_name}/RandomSeedSelectionQA', do_export=True, db=Database(), num_ctx_records=num_ctx_records, ctx_per_record=ctx_per_record, decode_batch_size=4))
 
 # Baselines
-generators.append(RandomVertexQAGenerator(export_path='output/RandomVertexQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=5, decode_batch_size=4, explainqar_results_fp='output/CRIKEY_QAG/qas.json'))
-generators.append(SlidingWindowQAGenerator(export_path='output/SlidingWindowQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=10, decode_batch_size=4, explainqar_results_fp='output/CRIKEY_QAG/qas.json', window_size=500))
-generators.append(DRQAGen(export_path='output/DRQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=10, decode_batch_size=4, explainqar_results_fp='output/CRIKEY_QAG/qas.json', chunk_size=500, k=2))
+generators.append(RandomVertexQAGenerator(export_path=f'{output_folder_name}/RandomVertexQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=10, decode_batch_size=4, explainqar_results_fp=f'{output_folder_name}/CRIKEY_QAG/qas.json'))
+generators.append(SlidingWindowQAGenerator(export_path=f'{output_folder_name}/SlidingWindowQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=ctx_per_record, decode_batch_size=4, explainqar_results_fp=f'{output_folder_name}/CRIKEY_QAG/qas.json', window_size=500))
+generators.append(DRQAGen(export_path=f'{output_folder_name}/DRQA', do_export=True, db=Database(), num_ctx_records=1000, ctx_per_record=ctx_per_record, decode_batch_size=4, explainqar_results_fp=f'{output_folder_name}/CRIKEY_QAG/qas.json', chunk_size=500, k=2))
 
 for generator in generators:
    generator.execute(model=model_dict.create()['claude'])
